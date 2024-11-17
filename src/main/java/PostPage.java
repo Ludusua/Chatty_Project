a1d7229c9032388f84d021bda12088d173b63241
Vitalii <virthunter@gmail.com>
2024-11-17 13:02:23 +0200

    added FILE_SEPARATOR

--- src/main/java/PostPage.java
+++ src/main/java/PostPage.java
@@ -4,10 +4,13 @@ import org.openqa.selenium.support.FindBy;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PostPage extends BasePage{
+public class PostPage extends BasePage {
     public PostPage(WebDriver driver) {
         super(driver);
     }
+
+    private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+    private static final String ABSOLUTEPATH = "C:"+ FILE_SEPARATOR +"Users"+FILE_SEPARATOR +"Dell"+FILE_SEPARATOR +"IdeaProjects"+FILE_SEPARATOR +"Chatty_Project"+FILE_SEPARATOR +"src"+FILE_SEPARATOR +"main"+FILE_SEPARATOR +"resources"+FILE_SEPARATOR +"books.png";
     @FindBy(xpath = "//input[@placeholder='Title']")
     private WebElement titleInputField;
     @FindBy(xpath = "//*[@placeholder='Description']")
@@ -26,7 +29,7 @@ public class PostPage extends BasePage{
     @FindBy(xpath = "//*[@data-test='uploaded-image']")
     private WebElement uploadedImage;
 
-    public void waitForLoadingAllFields(){
+    public void waitForLoadingAllFields() {
         getWait().forVisibility(titleInputField);
         assertTrue(titleInputField.isDisplayed());
         getWait().forVisibility(descriptionInputField);
@@ -34,37 +37,42 @@ public class PostPage extends BasePage{
         getWait().forVisibility(contentDescriptionInputField);
         assertTrue(contentDescriptionInputField.isDisplayed());
     }
-    public void waitForLoadingImage(){
+
+    public void waitForLoadingImage() {
         getWait().forVisibility(uploadedImage);
         assertTrue(uploadedImage.isDisplayed());
     }
 
-    public void setTitleInputField(String title){
+    public void setTitleInputField(String title) {
         titleInputField.sendKeys(title);
     }
-    public void setDescriptionInputField(String description){
+
+    public void setDescriptionInputField(String description) {
         descriptionInputField.sendKeys(description);
     }
-    public void setContentDescriptionInputField(String contentDescription){
+
+    public void setContentDescriptionInputField(String contentDescription) {
         contentDescriptionInputField.sendKeys(contentDescription);
     }
-    public void setPostData(String title,String description,String contentDescription){
+
+    public void setPostData(String title, String description, String contentDescription) {
         titleInputField.sendKeys(title);
         descriptionInputField.sendKeys(description);
         contentDescriptionInputField.sendKeys(contentDescription);
     }
 
-    public void clickOnTheSaveAsDraftSwitcher(){
+    public void clickOnTheSaveAsDraftSwitcher() {
         saveAsDraftSwitcher.isDisplayed();
         saveAsDraftSwitcher.click();
     }
-    public void clickSubmitButton(){
+
+    public void clickSubmitButton() {
         submitButton.isDisplayed();
         submitButton.click();
     }
-    public void uploadImage(){
-        String filePath = "C:\\Users\\Dell\\Downloads\\books.png";
-        imageUploadField.sendKeys(filePath);
+
+    public void uploadImage() {
+        imageUploadField.sendKeys(ABSOLUTEPATH);
     }
 
 }
